d3bfaf245f2ddebe6b41d80ac75514a6f815e5a4
khoahd <khoahd@runsystem.net>
2018-06-19 08:45:30 +0700

    remove unused files for running test

--- test/contracts/TestPerformanceContract.sol
+++
@@ -1,5 +0,0 @@
-pragma solidity ^0.4.17;
-
-contract TestPerformanceContract {
-    bytes32 constant CONTRACT_NAME = bytes32("TestPerformanceContract");
-}
--- test/migrations/2_deploy_cns.js
+++
@@ -1,9 +0,0 @@
-const config = require('../config.json'),
-    AddressGroup_v1 = artifacts.require('../../node_modules/zcom-contracts/contracts/AddressGroup.sol'),
-    ContractNameService = artifacts.require('./ContractNameService.sol');
-
-module.exports = function(deployer, network, accounts) {
-    deployer.deploy(ContractNameService).then(function() {
-        return ContractNameService.at(config.gmoCns).getLatestContract('AddressGroup');
-    });
-}
